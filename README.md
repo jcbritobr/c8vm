8742457a1fc4409896bacaefed07026bdfc2dea9
jcbritobr <jcbritobr@gmail.com>
2021-12-14 20:15:56 -0300

    Enhance documentation.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-### C8VM is a chip 8 virtual machine built using Go language.
+## C8VM is a chip 8 virtual machine built using Go language.
+
+### References
+* [Cowgod's CHIP-8 reference](http://devernay.free.fr/hacks/chip8/C8TECH10.HTM)
--- cpu/cpu_test.go
+++ cpu/cpu_test.go
@@ -19,6 +19,14 @@ func TestShouldLoadROM(t *testing.T) {
 	}
 	assert.Equal(t, 353, n, "353 should be read as the demo has 353 bytes")
 	for i := 0x50; i < 0x200; i++ {
-		assert.Equal(t, uint8(0), cpu.memory[i], "First 512 bytes must be zero, where vm works")
+		assert.Equal(t, uint8(0), cpu.memory[i], "First 512 bytes must be zero, as this is vm area")
+	}
+}
+
+func TestLoadROMShouldFailWithWrongFilename(t *testing.T) {
+	cpu := NewCPU()
+	_, err := cpu.LoadROM("DummyRom")
+	if err == nil {
+		assert.Fail(t, "Error cant be nil")
 	}
 }
